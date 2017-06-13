a9828721fdfbfc8a6bd62f56f45267ec916d5f41
Christopher Otto <chris.otto6@gmail.com>
2017-06-13 17:33:07 -0600

    Adding template and Week 2 base project

---
+++ Week2/Week2/Week2.cpp
@@ -0,0 +1,13 @@
+// Week1.cpp : main project file.
+
+#include "info.h"
+#include "PersonType.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	info myInfo;
+
+	myInfo.identity("", __DATE__);
+}
