bb5913417591d13f7657b1a3ac96f5e90e9bd3c3
Chris <chris.otto6@gmail.com>
2017-07-16 11:29:58 -0500

    Initial commit for week 6 assignment

---
+++ Week6/Week6/Main.cpp
@@ -0,0 +1,15 @@
+// Main.cpp contains the main project logic for the assignment
+
+#include "info.h"
+#include <iostream>
+
+void main()
+{
+	// Student info
+	info myInfo;
+	myInfo.identity("", __DATE__);
+
+
+	cin.ignore();
+	system("PAUSE");
+}
---
+++ Week6/Week6/info.h
@@ -0,0 +1,21 @@
+//Programmer:  Chris Otto
+//Purpose   :  Include this header file in all future assignments.  Will display programmers and assignment info.
+
+#ifndef INFO_H
+#define INFO_H
+
+#include <string>
+
+using namespace std;
+
+class info
+{
+private:
+	string programmer;
+	string instructor;
+public:
+	info();
+	void identity(string assignment, string compileDate);
+	~info();
+};
+#endif
